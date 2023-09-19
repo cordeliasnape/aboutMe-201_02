4d2c43dbaac11d731e3ca18c93249e4e902b9bf3
Cordelia Snape <snapecordelia@gmail.com>
2023-09-19 11:52:34 +0100

    add question 1

---
+++ script.js
@@ -0,0 +1,10 @@
+let answer1 = confirm("Are you ready???");
+if (answer1 === true) {
+  username = prompt("Amazing!\nWhat's your name?");
+  console.log("You're ready to rumble!");
+} else {
+  username = prompt("Boooo!!!\nWhat's your name?");
+  console.log("You're not ready to rumble :(");
+}
+let userName = prompt;
+console.log(username);
