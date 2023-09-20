5720274cd20eefc6a5aeff5bce357be5b85323db
Cordelia Snape <snapecordelia@gmail.com>
2023-09-20 17:42:21 +0100

    saving b4 i break my laptop :)

--- JavaScript/seventhQuestion.js
+++ JavaScript/seventhQuestion.js
@@ -1,33 +1,26 @@
-alert(
-  "I have a lucky number between 1-10\nCan you guess it?\nYou have 4 tries."
-);
+let favFood = [
+  "caesar salad",
+  "pizza",
+  "tteokbokki",
+  "hot pot",
+  "summer rolls",
+  "jianbing",
+  "seafood pasta",
+  "chinese lamb skewers",
+  "tacos",
+  "fried chicken",
+];
 
-let answer = 8;
-let guess = prompt("Can you guess it?");
+alert("Now it's time for a quiz!");
 
-for (i = 0; i < 3; i++) {
-  console.log(i);
+let foodAns = prompt("Name one of my favourite foods!").toLowerCase();
 
-  if (guess === answer) {
-    alert("Correct!");
-    console.log(guess);
-    break;
-  } else {
-    console.log(guess);
-    guess = prompt("Guess again!");
+for (let i = 0; i < favFood.length; i++) {
+  // console.log("checking");
+  if (foodAns == favFood[i]) {
+    // console.log("found");
+    alert(
+      "Well done!\nAll of the answers you could have guessed correctly were: caesar salad, pizza, tteokbokki, hot pot, summer rolls, jianbing, seafood pasta, Chinese lamb skewers, tacos or fried chicken"
+    );
   }
 }
-
-//   if (guess <= 7) {
-//     alert("Higher!");
-//     guess();
-//   } else if (guess >= 9) {
-//     alert("Lower!");
-//     guess();
-//   while (guess !== answer) {
-//     if (guess === answer) {
-//       alert("Correct!");
-//       break;
-//     } else {
-//       alert("Try again!");
-//     }
--- JavaScript/sixthQuestion.js
+++ JavaScript/sixthQuestion.js
@@ -1,16 +1,30 @@
-let guess = prompt(
+alert(
   "I have a lucky number between 1-10\nCan you guess it?\nYou have 4 tries."
 );
-for (i = 0; i < 4; i++) {
-  console.log(i);
+
+let answer = 8;
+let guess = prompt("Can you guess it?");
+
+for (i = 0; i < 3; i++) {
+  if (answer == guess) {
+    alert("Correct!");
+    console.log(guess);
+    break;
+  }
+  // else if (guess <= 7) {
+  //   guess = prompt("Higher!!");
+  //   guess();
+  // }
+  else {
+    guess = prompt("Guess again!");
+    console.log(guess);
+  }
 }
-if (guess <= 7) {
-  alert("Higher!");
-  // guess();
-} else if (guess >= 9) {
-  alert("Lower!");
-  // guess();
-}
-while (guess !== 8) {
-  alert("Try again!");
-}
+
+//   if (guess <= 7) {
+//     alert("Higher!");
+//     guess();
+
+//   } else if (guess >= 9) {
+//     alert("Lower!");
+//     guess();
