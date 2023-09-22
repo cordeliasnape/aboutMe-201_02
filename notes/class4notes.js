99c041062dd19d2c3af94aabac67e5fcf96279ec
Cordelia Snape <snapecordelia@gmail.com>
2023-09-22 16:44:52 +0100

    added colours and links

---
+++ notes/class4notes.js
@@ -0,0 +1,40 @@
+let myArray = ["1", "2", "3"];
+
+let myInfo = ["sam", 24, "true"[("darcy", "charlie", "einstein")]];
+
+for (i = 0; i > myIndexExample.length; i++) {
+  //initialisation; condition; afterthought
+  console.log(myIndexExample[i]); // we can use i to print all of the things in the array - can make it a full sentence
+}
+
+//
+
+//fucntion expressions, function declaration, etc
+//parameters are temporary values, they're placeholders for other data types declared in arguments
+
+//a function declaration is:
+//returning it ends the function, nothing after it happens
+
+function myFunc(a, b) {
+  return a + b //we need to return the values bc we haven't told ot to return the value back to me, no return is no answer
+}
+
+myFunc (34, 78) // we can set the value of the function when we use argements when declaring a function
+
+function sum(a,b) {
+  let output = 0
+  for (let i=0; i<5; i++) {
+    return a + b
+  }
+}
+
+function sum(a,b) {
+  let output = 0
+  for (let i=0; i<5; i++) {
+    output += + a + b  
+  }
+  return output
+}
+ // loops multiple times, adding 15 5 times
+
+
